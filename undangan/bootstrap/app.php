043e534b5218451b7dc47b69e84eaf4ca9b4e9d9
albertwujaya <albertwujaya101@gmail.com>
2026-05-21 09:15:37 +0700

    Configure Laravel for Vercel deployment

--- undangan/bootstrap/app.php
+++ undangan/bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -16,3 +16,20 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
+if (isset($_ENV['VERCEL'])) {
+    $storagePath = '/tmp/storage';
+    $dirs = [
+        $storagePath . '/framework/views',
+        $storagePath . '/framework/sessions',
+        $storagePath . '/framework/cache',
+    ];
+    foreach ($dirs as $dir) {
+        if (!is_dir($dir)) {
+            mkdir($dir, 0755, true);
+        }
+    }
+    $app->useStoragePath($storagePath);
+}
+
+return $app;
